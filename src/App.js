95c5d252f068edb8eb0dad53b497fd90b2cbc4fd
yap8 <nebozhenko.arseny@gmail.com>
2021-10-29 15:14:18 +0300

    added pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { Footer, Header } from "./components";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import GlobalStyle from "./GlobalStyles";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Router>
       <GlobalStyle />
       <Header />
-      <Switch>
-        <Route exact path="/">
-
-        </Route>
-      </Switch>
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
       <Footer />
     </Router>
   );
---
+++ src/pages/Pages.js
@@ -0,0 +1,9 @@
+const Pages = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Pages
