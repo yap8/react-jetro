5bb9605a791e6a360c827ec783d2982d63f7f7db
yap8 <nebozhenko.arseny@gmail.com>
2021-11-08 17:20:58 +0300

    added map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,22 @@
-const Map = () => {
+import { GoogleMap, withGoogleMap, withScriptjs, Marker } from 'react-google-maps'
+
+const Map = withScriptjs(withGoogleMap(() => {
   return (
     <div>
-      
+      <GoogleMap
+        defaultZoom={10}
+        defaultCenter={{
+          lat: 52.146973,
+          lng: -106.647034
+        }}
+      >
+        <Marker position={{
+          lat: 52.146973,
+          lng: -106.647034
+        }} />
+      </GoogleMap>
     </div>
   )
-}
+}))
 
 export default Map
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,7 +6,12 @@ const Contact = () => {
       <SectionTitle>Contact us</SectionTitle>
       <ContentWrapper>
         <PageContent>
-          <Map />
+          <Map
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBsUXs5jv6tud_QCJSYW8TbyS1l5IP63Xc`}
+            loadingElement={<div style={{ height: '100%' }} />}
+            containerElement={<div style={{ height: 400, marginBottom: 36 }} />}
+            mapElement={<div style={{ height: '100%' }} />}
+          />
           <Form />
         </PageContent>
         <Aside />
