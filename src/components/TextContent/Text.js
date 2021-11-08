c02bb0975847c0b36acb943227b24a5e798e1f98
yap8 <nebozhenko.arseny@gmail.com>
2021-11-09 00:51:45 +0300

    added quote variant for text component

--- src/components/TextContent/Text.js
+++ src/components/TextContent/Text.js
@@ -4,6 +4,7 @@ const Wrapper = styled.div`
   font-size: 12px;
   color: #414141;
   line-height: 18px;
+  position: relative;
   &:not(:last-child) {
     margin-right: 15px;
   }
@@ -15,10 +16,38 @@ const Wrapper = styled.div`
   }
 `
 
+const Quote = styled.div`
+  padding-left: 77px;
+  font-style: italic;
+  &::before {
+    content: '';
+    display: block;
+    width: 8px;
+    height: 95%;
+    left: 50px;
+    top: 50%;
+    transform: translateY(-50%);
+    background-color: #e8663c;
+    position: absolute;
+  }
+  @media screen and (max-width: 940px) {
+    padding-left: 20px;
+    &::before {
+      left: 0;
+    }
+  }
+`
+
 const Text = ({ children, quote }) => {
   return (
     <Wrapper>
-      {children}
+      {quote ? (
+        <Quote>
+          {children}
+        </Quote>
+      ) : (
+        children
+      )}
     </Wrapper>
   )
 }
