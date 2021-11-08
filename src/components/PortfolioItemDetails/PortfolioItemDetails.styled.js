612f049def4db2ad02e8648d7dc2d2fa4747930c
yap8 <nebozhenko.arseny@gmail.com>
2021-11-08 21:16:03 +0300

    made portfolio details responsive

--- src/components/PortfolioItemDetails/PortfolioItemDetails.styled.js
+++ src/components/PortfolioItemDetails/PortfolioItemDetails.styled.js
@@ -3,15 +3,24 @@ import { Container } from "../../GlobalStyles";
 
 export const Wrapper = styled.section`
   margin-bottom: 40px;
+  @media screen and (max-width: 940px) {
+    margin-bottom: 20px;
+  }
 `
 
 export const Inner = styled(Container)``
 
 export const HeroImageWrapper = styled.div`
-  height: 400px;
+  min-height: 400px;
   overflow: hidden;
   position: relative;
   margin-bottom: 20px;
+  @media screen and (max-width: 940px) {
+    min-height: 320px;
+  }
+  @media screen and (max-width: 720px) {
+    min-height: 220px;
+  }
 `
 
 export const HeroImage = styled.img`
@@ -19,7 +28,8 @@ export const HeroImage = styled.img`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 100%;
+  min-width: 100%;
+  min-height: 400px;
 `
 
 export const Text = styled.div`
